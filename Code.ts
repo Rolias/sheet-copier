64c4819413f95302c454ccf66e5328fcbc24e6dd
Tod Gentille <tod-gentille@Tods-Gray-Ghost.lan>
2018-10-21 15:42:29 -0700

    cleaned up tslint errors

--- Code.ts
+++ Code.ts
@@ -1,13 +1,31 @@
-// @ts-check 
+//====================
+//PUBLIC FUNCTIONS 
+//====================
 
-//====================
-// PRIVATE FUNCTIONS
-//====================
-function getMostRecentFileInFolder_(folderId: string): DriveFile {
-  const folder = DriveApp.getFolderById(folderId);
-  const files = folder.getFiles();
-  const file = files.next();
-  return file;
+/**
+ * Takes the first file in the folder (the most recent one) and copies all the data from
+ * the first sheet on that Spreadsheet to the first sheet on the destination spreadsheet
+ * @param {string} srcFolderId - the id string of the folder that has the spreadsheet acting as the source
+ * @param {string} destSheetId  - the id string of the spreadsheet that is the destination
+ * @return {void}
+ * exported copyDataFromSourceFolderToDestSheet
+ */
+function copyDataFromSourceFolderToDestSheet(srcFolderId: string, destSheetId: string): void { // eslint-disable-line no-unused-vars
+  const file = getMostRecentFileInFolder_(srcFolderId);
+  const file_sheet = getFirstSheetFromFile_(file);
+  const data = getDataFromSheet(file_sheet);
+  const sheet = getFirstSheetFromId_(destSheetId);
+  // copyDataToDestSheet_(data, sheet);
+  copyDataToSheet(data, sheet);
+}
+/**
+ * Get all the data from the passed sheet reference
+ * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet reference to google sheet 
+ * @return {Object[][]} the sheet's data
+ */
+function getDataFromSheet(sheet: GoogleAppsScript.Spreadsheet.Sheet): DataValues {
+  const fullDataRange = sheet.getDataRange();
+  return fullDataRange.getValues();
 }
 
 function getFirstSheetFromFile_(file: DriveFile): Sheet {
@@ -25,45 +43,11 @@ function getFirstSheetFromId_(id: string): Sheet {
   return ss.getSheets()[0];
 }
 
-function copyDataToDestSheet_(data: DataValues, sheet: Sheet): void {
-  const rows = data.length;
-  const cols = data[0].length;
-  sheet.getRange(1, 1, rows, cols).setValues(data);
-}
-
-//====================
-//PUBLIC FUNCTIONS 
-//====================
-
-/**
- * Get all the data from the passed sheet reference
- * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet reference to google sheet 
- * @return {Object[][]} the sheet's data
- */
-function getDataFromSheet(sheet: GoogleAppsScript.Spreadsheet.Sheet): DataValues {
-  const fullDataRange = sheet.getDataRange();
-  return fullDataRange.getValues();
-}
-
-/**
- * Using the active spreadsheet, return the named sheet
- * @param {string} sheetName the name of the sheet we want returned
- * @return {Sheet} 
- */
-function getActiveSheetByName(sheetName: string): Sheet {// eslint-disable-line no-unused-vars
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  return ss.getSheetByName(sheetName);
-}
-
-/**
- * Return a named sheet from a spreadsheet. 
- * @param {string} id of the spreadsheet 
- * @param {*} name of the sheet of interest
- * @return {Sheet} a google sheet reference
- */
-function getNamedSheetFromId(id: string, name: string): Sheet {// eslint-disable-line no-unused-vars
-  const ss = SpreadsheetApp.openById(id);
-  return ss.getSheetByName(name);
+function getMostRecentFileInFolder_(folderId: string): DriveFile {
+  const folder = DriveApp.getFolderById(folderId);
+  const files = folder.getFiles();
+  const file = files.next();
+  return file;
 }
 
 /**
@@ -79,18 +63,25 @@ function copyDataToSheet(data: object[][], sheet: GoogleAppsScript.Spreadsheet.S
 }
 
 /**
- * Takes the first file in the folder (the most recent one) and copies all the data from
- * the first sheet on that Spreadsheet to the first sheet on the destination spreadsheet
- * @param {string} srcFolderId - the id string of the folder that has the spreadsheet acting as the source
- * @param {string} destSheetId  - the id string of the spreadsheet that is the destination
- * @return {void}
- * exported copyDataFromSourceFolderToDestSheet
+ * Using the active spreadsheet, return the named sheet
+ * @param {string} sheetName the name of the sheet we want returned
+ * @return {Sheet} 
  */
-function copyDataFromSourceFolderToDestSheet(srcFolderId: string, destSheetId: string): void { // eslint-disable-line no-unused-vars
-  const file = getMostRecentFileInFolder_(srcFolderId);
-  const file_sheet = getFirstSheetFromFile_(file);
-  const data = getDataFromSheet(file_sheet);
-  const sheet = getFirstSheetFromId_(destSheetId);
-  copyDataToDestSheet_(data, sheet);
+function getActiveSheetByName(sheetName: string): Sheet {// eslint-disable-line no-unused-vars
+  let ss = SpreadsheetApp.getActiveSpreadsheet();
+  return ss.getSheetByName(sheetName);
 }
+
+/**
+ * Return a named sheet from a spreadsheet. 
+ * @param {string} id of the spreadsheet 
+ * @param {*} name of the sheet of interest
+ * @return {Sheet} a google sheet reference
+ */
+function getNamedSheetFromId(id: string, name: string): Sheet {// eslint-disable-line no-unused-vars
+  const ss = SpreadsheetApp.openById(id);
+  return ss.getSheetByName(name);
+}
+
+
 
--- build/Code.js
+++ build/Code.js
@@ -1,13 +1,31 @@
 "use strict";
-// @ts-check 
 //====================
-// PRIVATE FUNCTIONS
+//PUBLIC FUNCTIONS 
 //====================
-function getMostRecentFileInFolder_(folderId) {
-    var folder = DriveApp.getFolderById(folderId);
-    var files = folder.getFiles();
-    var file = files.next();
-    return file;
+/**
+ * Takes the first file in the folder (the most recent one) and copies all the data from
+ * the first sheet on that Spreadsheet to the first sheet on the destination spreadsheet
+ * @param {string} srcFolderId - the id string of the folder that has the spreadsheet acting as the source
+ * @param {string} destSheetId  - the id string of the spreadsheet that is the destination
+ * @return {void}
+ * exported copyDataFromSourceFolderToDestSheet
+ */
+function copyDataFromSourceFolderToDestSheet(srcFolderId, destSheetId) {
+    var file = getMostRecentFileInFolder_(srcFolderId);
+    var file_sheet = getFirstSheetFromFile_(file);
+    var data = getDataFromSheet(file_sheet);
+    var sheet = getFirstSheetFromId_(destSheetId);
+    // copyDataToDestSheet_(data, sheet);
+    copyDataToSheet(data, sheet);
+}
+/**
+ * Get all the data from the passed sheet reference
+ * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet reference to google sheet
+ * @return {Object[][]} the sheet's data
+ */
+function getDataFromSheet(sheet) {
+    var fullDataRange = sheet.getDataRange();
+    return fullDataRange.getValues();
 }
 function getFirstSheetFromFile_(file) {
     var ss = SpreadsheetApp.open(file);
@@ -22,22 +40,22 @@ function getFirstSheetFromId_(id) {
     var ss = SpreadsheetApp.openById(id);
     return ss.getSheets()[0];
 }
-function copyDataToDestSheet_(data, sheet) {
+function getMostRecentFileInFolder_(folderId) {
+    var folder = DriveApp.getFolderById(folderId);
+    var files = folder.getFiles();
+    var file = files.next();
+    return file;
+}
+/**
+ * Copy data into sheet, using size of data to get proper range
+ * @param {Object[][]} data double array from a google sheet
+ * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet - the destination sheet
+ * @return {void}
+ */
+function copyDataToSheet(data, sheet) {
     var rows = data.length;
     var cols = data[0].length;
     sheet.getRange(1, 1, rows, cols).setValues(data);
-}
-//====================
-//PUBLIC FUNCTIONS 
-//====================
-/**
- * Get all the data from the passed sheet reference
- * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet reference to google sheet
- * @return {Object[][]} the sheet's data
- */
-function getDataFromSheet(sheet) {
-    var fullDataRange = sheet.getDataRange();
-    return fullDataRange.getValues();
 }
 /**
  * Using the active spreadsheet, return the named sheet
@@ -57,30 +75,4 @@ function getActiveSheetByName(sheetName) {
 function getNamedSheetFromId(id, name) {
     var ss = SpreadsheetApp.openById(id);
     return ss.getSheetByName(name);
-}
-/**
- * Copy data into sheet, using size of data to get proper range
- * @param {Object[][]} data double array from a google sheet
- * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet - the destination sheet
- * @return {void}
- */
-function copyDataToSheet(data, sheet) {
-    var rows = data.length;
-    var cols = data[0].length;
-    sheet.getRange(1, 1, rows, cols).setValues(data);
-}
-/**
- * Takes the first file in the folder (the most recent one) and copies all the data from
- * the first sheet on that Spreadsheet to the first sheet on the destination spreadsheet
- * @param {string} srcFolderId - the id string of the folder that has the spreadsheet acting as the source
- * @param {string} destSheetId  - the id string of the spreadsheet that is the destination
- * @return {void}
- * exported copyDataFromSourceFolderToDestSheet
- */
-function copyDataFromSourceFolderToDestSheet(srcFolderId, destSheetId) {
-    var file = getMostRecentFileInFolder_(srcFolderId);
-    var file_sheet = getFirstSheetFromFile_(file);
-    var data = getDataFromSheet(file_sheet);
-    var sheet = getFirstSheetFromId_(destSheetId);
-    copyDataToDestSheet_(data, sheet);
 }
